d0e899f2254bd2c5429bddc7f71e4c314d248272
Mark T. Holder <mtholder@gmail.com>
2017-04-22 17:02:51 -0500

    closer

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+virtualenv venv || exit
+source venv/bin/activate || exit
+git clone https://github.com/mtholder/peyotl.git || exit
+
+cd peyotl || exit
+git checkout dev-missing-illustrations || exit
+pip install -r requirements.txt || exit
+python setup.py develop || exit
+cd - 2>/dev/null || exit
+
+python setup.py develop || exit
+
+echo
+echo 'Taxalotl installed, but you will still need to run:'
+echo '    source venv/bin/activate'
+echo 'in your shell before you can access it.'
+
+if ! test -f taxalotl.conf
+then
+    cp taxalotl.conf.example taxalotl.conf || exit
+    echo 'You will also need to edit "taxalotl.conf" to choose locations for the data'
+fi
+
+echo
+echo "You almost certainly want to run something like:"
+echo "   export PEYOTL_CONFIG_FILE=\"$PWD/recommended-peyotl-conf.ini\""
+echo "in your bash shell (or the equivalent if you do not use bash for some reason"
+echo "so that you can see the logging messages."
+
---
+++ setup.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+import sys
+import os
+
+# setup.py largely based on
+#   http://hynek.me/articles/sharing-your-labor-of-love-pypi-quick-and-dirty/
+# Also see Jeet Sukumaran's DendroPy
+
+###############################################################################
+# setuptools/distutils/etc. import and configuration
+try:
+    import ez_setup
+    try:
+        ez_setup_path = " ('" + os.path.abspath(ez_setup.__file__) + "')"
+    except OSError:
+        ez_setup_path = ""
+    sys.stderr.write("using ez_setup%s\n" % ez_setup_path)
+    ez_setup.use_setuptools()
+    import setuptools
+    try:
+        setuptools_path = " ('" + os.path.abspath(setuptools.__file__) + "')"
+    except OSError:
+        setuptools_path = ""
+    sys.stderr.write("using setuptools%s\n" % setuptools_path)
+    from setuptools import setup, find_packages
+except ImportError as e:
+    sys.stderr.write("using distutils\n")
+    from distutils.core import setup
+
+    sys.stderr.write("using canned package list\n")
+    PACKAGES = ['taxalotl',
+               ]
+    EXTRA_KWARGS = {}
+else:
+    sys.stderr.write("searching for packages\n")
+    PACKAGES = find_packages()
+    EXTRA_KWARGS = dict(
+        include_package_data=True,
+        test_suite="taxalotl.test"
+    )
+
+EXTRA_KWARGS["zip_safe"] = False
+ENTRY_POINTS = {}
+EXTRA_KWARGS['scripts'] = ['taxalotl-cli.py',
+                          ]
+setup(
+    name='taxalotl',
+    version='0.0dev',  # sync with __version__ in peyotl/__init__.py
+    description='Taxonomy merging tool',
+    long_description=(open('README.md').read()),
+    url='https://github.com/mtholder/taxalotl',
+    license='BSD',
+    author='Mark T. Holder',
+    py_modules=['taxalotl'],
+    install_requires=['setuptools',
+                      'requests>=2.2.1',
+                     ],
+    packages=PACKAGES,
+    entry_points=ENTRY_POINTS,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
+    **EXTRA_KWARGS
+)
