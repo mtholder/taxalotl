4c5129084d8efc502389800f42dcadf18498afa2
Mark T. Holder <mtholder@gmail.com>
2018-11-24 13:29:08 -0600

    updating setup.py

--- setup.py
+++ setup.py
@@ -1,68 +1,113 @@
 #!/usr/bin/env python
-import sys
+# -*- coding: utf-8 -*-
+# setup.py based on https://github.com/kennethreitz/setup.py
+# by Kenneth Reitz. Its license is the following MIT license.
+
+# Copyright <YEAR> <COPYRIGHT HOLDER>
+
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+##############################################################################
+import io
 import os
+import sys
+from setuptools import find_packages, setup, Command
 
-# setup.py largely based on
-#   http://hynek.me/articles/sharing-your-labor-of-love-pypi-quick-and-dirty/
-# Also see Jeet Sukumaran's DendroPy
 
-###############################################################################
-# setuptools/distutils/etc. import and configuration
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        pd = os.path.join(here, 'dist')
+        if os.path.exists(pd):
+            self.status('Renaming moving previous dist to prevdist')
+            prevd = os.path.join(here, 'prevdist')
+            os.path.rename(pd, prevd)
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+        sys.exit()
+
+# Package meta-data.
+NAME = 'taxalotl'
+DESCRIPTION = 'Taxonomy updating tool'
+URL = 'https://github.com/mtholder/taxalotl'
+EMAIL = 'mtholder@gmail.com'
+AUTHOR = 'Mark T. Holder'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = None
+
+REQUIRED = ['setuptools', 'requests>=2.2.1', 'ez_setup', ]
+EXTRAS = {}
+here = os.path.abspath(os.path.dirname(__file__))
+
 try:
-    # noinspection PyUnresolvedReferences
-    import ez_setup
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
 
-    try:
-        ez_setup_path = " ('" + os.path.abspath(ez_setup.__file__) + "')"
-    except OSError:
-        ez_setup_path = ""
-    sys.stderr.write("using ez_setup%s\n" % ez_setup_path)
-    ez_setup.use_setuptools()
-    # noinspection PyUnresolvedReferences
-    import setuptools
-
-    try:
-        setuptools_path = " ('" + os.path.abspath(setuptools.__file__) + "')"
-    except OSError:
-        setuptools_path = ""
-    sys.stderr.write("using setuptools%s\n" % setuptools_path)
-    # noinspection PyUnresolvedReferences
-    from setuptools import setup, find_packages
-except ImportError as e:
-    sys.stderr.write("using distutils\n")
-    from distutils.core import setup
-
-    sys.stderr.write("using canned package list\n")
-    PACKAGES = ['taxalotl',
-                ]
-    EXTRA_KWARGS = {}
+about = {}
+if not VERSION:
+    with open(os.path.join(here, NAME, '__version__.py')) as f:
+        exec(f.read(), about)
 else:
-    sys.stderr.write("searching for packages\n")
-    PACKAGES = find_packages()
-    # noinspection PyTypeChecker
-    EXTRA_KWARGS = dict(
-        include_package_data=True,
-        test_suite="taxalotl.test"
-    )
+    about['__version__'] = VERSION
 
-EXTRA_KWARGS["zip_safe"] = False
-ENTRY_POINTS = {}
-EXTRA_KWARGS['scripts'] = ['taxalotl-cli.py',
-                           ]
 setup(
-    name='taxalotl',
-    version='0.0dev',  # sync with __version__ in peyotl/__init__.py
-    description='Taxonomy merging tool',
-    long_description=(open('README.md').read()),
-    url='https://github.com/mtholder/taxalotl',
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
+    scripts=['taxalotl-cli.py', ],
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
     license='BSD',
-    author='Mark T. Holder',
-    py_modules=['taxalotl'],
-    install_requires=['setuptools',
-                      'requests>=2.2.1', 'ez_setup',
-                      ],
-    packages=PACKAGES,
-    entry_points=ENTRY_POINTS,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
@@ -70,10 +115,12 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
     ],
-    **EXTRA_KWARGS
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
