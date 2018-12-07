421b9cbc2c81319cbed7cfad3e355153395099e0
Mark T. Holder <mtholder@gmail.com>
2018-12-07 10:01:19 -0600

    tweaks to install script

--- Tutorial.md
+++ Tutorial.md
@@ -3,3 +3,6 @@ See [README.md](./README.md) for general documentation.
 
 ## Installation
 
+    git clone https://github.com/mtholder/taxalotl.git
+    cd taxalotl
+    
--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-virtualenv venv || exit
-source venv/bin/activate || exit
+virtualenv -p$(which python3) env || exit
+source env/bin/activate || exit
 git clone https://github.com/mtholder/peyotl.git || exit
 
 cd peyotl || exit
-git checkout taxalotl || exit
+git checkout -b taxalotl origin/taxalotl || exit
 pip install -r requirements.txt || exit
 python setup.py develop || exit
 cd - 2>/dev/null || exit
@@ -16,15 +16,21 @@ echo 'Taxalotl installed, but you will still need to run:'
 echo '    source venv/bin/activate'
 echo 'in your shell before you can access it.'
 
-if ! test -f taxalotl.conf
+usershome=$(readlink -f ~)
+fptd="${usershome}/.opentreeoflife/taxalotl/"
+if ! test -d "${fptd}"
 then
-    cp taxalotl.conf.example taxalotl.conf || exit
-    echo 'You will also need to edit "taxalotl.conf" to choose locations for the data'
+    mkdir -p "${fptd}" || exit
+fi
+
+if ! test -f "${fptd}/taxalotl.conf"
+then
+    cat taxalotl.conf.example | sed -e "s:ABS-PATH-TO-YOUR-HOME-DIR:${usershome}:" > "${fptd}/taxalotl.conf" || exit
 fi
 
 echo
 echo "You almost certainly want to run something like:"
 echo "   export PEYOTL_CONFIG_FILE=\"$PWD/recommended-peyotl-conf.ini\""
-echo "in your bash shell (or the equivalent if you do not use bash for some reason"
+echo "in your bash shell (or the equivalent if you do not use bash for some reason)"
 echo "so that you can see the logging messages."
 
