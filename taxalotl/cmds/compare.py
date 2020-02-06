9c2e700eb3d5b03a41ef233ed75aeab175931cd6
Mark T. Holder <mtholder@gmail.com>
2020-02-06 11:17:47 -0600

    sigh... better...

--- taxalotl/cmds/compare.py
+++ taxalotl/cmds/compare.py
@@ -58,19 +58,16 @@ def compare_taxonomies_in_dir(taxalotl_conf, tax_dir):
         res, ref_graph = res_graph_pair
         _write_report(out, ott_res.id, ott_graph, res_id, ref_graph)
 
-def _write_report(out, ott_id, ott_graph, res_id, ref_graph):
-    # ott_vstc = ott_graph.valid_specimen_based_taxa
-    # ref_vstc = ref_graph.valid_specimen_based_taxa
+def _init_bipartition_valid_taxa(ott_graph, ref_graph):
     ott_vn2tc = ott_graph.valid_name_to_taxon_concept_map
-    out.write('comparing {} to {}\n'.format(ott_id, res_id))
     ref_vn2tc = ref_graph.valid_name_to_taxon_concept_map
-    just_ott, both, just_ref = [], [], []
+    just_ott, valid_in_both, just_ref = [], [], []
     for ott_name, tax_con in ott_vn2tc.items():
         if not tax_con.is_specimen_based:
             continue
         ref_tc = ref_vn2tc.get(ott_name)
         if ref_tc:
-            both.append((ott_name, tax_con, ott_name, [ref_tc]))
+            valid_in_both.append((ott_name, tax_con, ott_name, [ref_tc]))
         else:
             just_ott.append((ott_name, tax_con))
     for ref_name, tax_con in ref_vn2tc.items():
@@ -78,31 +75,60 @@ def _write_report(out, ott_id, ott_graph, res_id, ref_graph):
             just_ref.append((ref_name, tax_con))
     just_ott.sort()
     just_ref.sort()
-    dnm, ott_unmatched = _gather_unmatched(just_ott, ref_graph)
-    diff_name_matched = dnm
-    dnm, ref_unmatched = _gather_unmatched(just_ref, ott_graph, rev_order=True)
-    diff_name_matched.extend(dnm)
-    both.extend(diff_name_matched)
+    ott_only_claims_valid, ott_unmatched = _gather_unmatched(just_ott, ref_graph)
+    ref_only_claims_valid, ref_unmatched = _gather_unmatched(just_ref, ott_graph, rev_order=True)
     ott_matched_set, ref_matched_set = set(), set()
-    for el in both:
+    for el in valid_in_both:
         ott_tc, ref_tc = el[1], el[3]
         ott_matched_set.add(ott_tc)
         if ref_tc is None:
             continue
-        m = ref_matched_set.update if isinstance(ref_tc, list) else ref_matched_set.add
-        m(ref_tc)
+        assert len(ref_tc) == 1
+        ref_matched_set.update(ref_tc)
     ott_unmatched = [i for i in ott_unmatched if i[1] not in ott_matched_set]
     ref_unmatched = [i for i in ref_unmatched if i[1] not in ref_matched_set]
+    bijection_with_1_invalid = []
+    one_ott_to_many = []
+    one_ref_to_many = []
+    for el in ott_only_claims_valid:
+        other_tc_list = el[-1]
+        if len(other_tc_list) > 1:
+            one_ott_to_many.append(el)
+        else:
+            bijection_with_1_invalid.append(el)
+    if ref_only_claims_valid:
+        other_tc_list = el[-1]
+        if len(other_tc_list) > 1:
+            one_ref_to_many.append(el)
+        else:
+            bijection_with_1_invalid.append(el)
+    return {'valid_in_both': valid_in_both, 'bijection_with_1_invalid': bijection_with_1_invalid,
+            'in_first_but_ambig': one_ott_to_many, 'in_second_but_ambig': one_ref_to_many,
+            'found_only_in_first': ott_unmatched, 'valid_only_in_first': ott_only_claims_valid,
+            'found_only_in_second': ref_unmatched, 'valid_only_in_second': ref_only_claims_valid,
+            }
+
+def _write_report(out, ott_id, ott_graph, res_id, ref_graph):
+    # ott_vstc = ott_graph.valid_specimen_based_taxa
+    # ref_vstc = ref_graph.valid_specimen_based_taxa
+    out.write('comparing {} to {}\n'.format(ott_id, res_id))
+    init_bipar = _init_bipartition_valid_taxa(ott_graph, ref_graph)
     # Report
-    out.write('{} only in  {} :\n'.format(len(just_ott), ott_id))
-    _write_tc_status(out, ott_unmatched)
-    out.write('{} only in  {} :\n'.format(len(just_ref), res_id))
-    _write_tc_status(out, ref_unmatched)
+    for key, id_to_show in [['found_only_in_first', ott_id], ['found_only_in_second', res_id]]:
+        blob = init_bipar[key]
+        out.write('{} only in  {} :\n'.format(len(blob), id_to_show))
+        _write_tc_status(out, blob)
+    for key, id_to_show in [['in_first_but_ambig', ott_id], ['in_second_but_ambig', res_id]]:
+        blob = init_bipar[key]
+        oid = ott_id if id_to_show == res_id else res_id
+        out.write('{} in  {} mapping to >1 in {} :\n'.format(len(blob), id_to_show, oid))
+        _write_tc_status(out, blob)
     out.write('Checking type of names of synonyms for {} \n'.format(ott_id))
     _check_syn_name_types(out, ott_graph)
     out.write('Checking type of names of synonyms for {} \n'.format(res_id))
     _check_syn_name_types(out, ref_graph)
-    _diagnose_higher_taxa(out, both, ott_id, ott_graph, res_id, ref_graph)
+    in_both = init_bipar['valid_in_both'] + init_bipar['bijection_with_1_invalid']
+    _diagnose_higher_taxa(out, in_both, ott_id, ott_graph, res_id, ref_graph)
 
 def _diagnose_higher_taxa(out, matched, ott_id, ott_graph, res_id, ref_graph):
     ott_tc_set, ref_tc_set = set(), set()
@@ -162,7 +188,6 @@ def _diagnose_higher_taxa(out, matched, ott_id, ott_graph, res_id, ref_graph):
             red_snarl.remove(td)
     snarl = red_snarl
 
-
     out.write('{} names in {} need to be split into multiple names in and {}:\n'.format(len(split), ott_id, res_id))
     _write_using_mapped_to(out, split)
     out.write('{} sets of names in {} need to be merged into names in and {}:\n'.format(len(merge), ott_id, res_id))
@@ -171,70 +196,7 @@ def _diagnose_higher_taxa(out, matched, ott_id, ott_graph, res_id, ref_graph):
     _write_using_mapped_to(out, snarl)
     out.write('{} names in {} map uniquely to a name in {}:\n'.format(len(uniq_map), ott_id, res_id))
     _write_using_mapped_to(out, uniq_map)
-    '''
-    out.write('{} names in {} map to multiple names in and {}:\n'.format(len(ref_to_many), res_id, ott_id))
-    n = 1
-    for ott_tc, ref_tc_set in ref_to_many.items():
-        otn = ott_tc.canonical_name.name
-        rtnl = [i.canonical_name.name for i in ref_tc_set]
-        rtnl.sort()
-        out.write('{} "{}" -> {}\n'.format(n, otn, rtnl))
-        n += 1
-    out.write('{} unambiguous matches in {} and {}:\n'.format(len(u_o2r), ott_id, res_id))
-    n = 1
-    for n, blob in enumerate(u_o2r):
-        ott_name, ref_name, ott_tc, ref_tc = blob
-        if ott_name == ref_name:
-            o = ''
-        else:
-            o = ' => "{}"'.format(ref_name)
-        out.write('{} "{}" {} '.format(1 + n, ott_name, o))
-        out.write('\n')
 
-    ott_vn2tc = ott_graph.valid_name_to_taxon_concept_map
-    out.write('comparing {} to {}\n'.format(ott_id, res_id))
-    ref_vn2tc = ref_graph.valid_name_to_taxon_concept_map
-    just_ott, both, just_ref = [], [], []
-    for ott_name, tax_con in ott_vn2tc.items():
-        if not tax_con.is_specimen_based:
-            continue
-        ref_tc = ref_vn2tc.get(ott_name)
-        if ref_tc:
-            both.append((ott_name, tax_con, ott_name, ref_tc))
-        else:
-            just_ott.append((ott_name, tax_con))
-    for ref_name, tax_con in ref_vn2tc.items():
-        if ref_name not in ott_vn2tc:
-            just_ref.append((ref_name, tax_con))
-    just_ott.sort()
-    just_ref.sort()
-    dnm, ott_unmatched = _gather_unmatched(just_ott, ref_graph)
-    diff_name_matched = dnm
-    dnm, ref_unmatched = _gather_unmatched(just_ref, ott_graph, rev_order=True)
-    diff_name_matched.extend(dnm)
-    both.extend(diff_name_matched)
-    ott_matched_set, ref_matched_set = set(), set()
-    for el in both:
-        ott_tc, ref_tc = el[1], el[3]
-        ott_matched_set.add(ott_tc)
-        if ref_tc is None:
-            continue
-        m = ref_matched_set.update if isinstance(ref_tc, list) else ref_matched_set.add
-        m(ref_tc)
-    ott_unmatched = [i for i in ott_unmatched if i[1] not in ott_matched_set]
-    ref_unmatched = [i for i in ref_unmatched if i[1] not in ref_matched_set]
-    # Report
-    out.write('{} only in  {} :\n'.format(len(just_ott), ott_id))
-    _write_tc_status(out, ott_unmatched)
-    out.write('{} only in  {} :\n'.format(len(just_ref), res_id))
-    _write_tc_status(out, ref_unmatched)
-    out.write('{} in {} and {}:\n'.format(len(both), ott_id, res_id))
-    _write_report_info_matched(out, both)
-    out.write('Checking type of names of synonyms for {} \n'.format(ott_id))
-    _check_syn_name_types(out, ott_graph)
-    out.write('Checking type of names of synonyms for {} \n'.format(res_id))
-    _check_syn_name_types(out, ref_graph)
-    '''
 
 def _write_using_mapped_to(out, mapping_set):
     for n, k in enumerate(mapping_set):
@@ -317,9 +279,14 @@ def _gather_unmatched(just_in, other_graph, rev_order=False):
 def _write_tc_status(out, name_tc_list):
     n = 1
     for blob in name_tc_list:
-        name = blob[0]
-        obj = blob[1]
-        out.write('{} '.format(n))
-        obj.explain(out)
-        out.write('\n')
+        if len(blob) == 2:
+            obj = blob[1]
+            out.write('  {} '.format(n))
+            obj.explain(out)
+            out.write('\n')
+        else:
+            matched_tc_list = blob[3]
+            vnlist = [i.valid_name for i in matched_tc_list]
+            o = '", "'.join([i.name for i in vnlist])
+            out.write('"{}" ambiguous between ["{}"]\n'.format(blob[0], o))
         n += 1
